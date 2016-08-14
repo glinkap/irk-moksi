999dffa40d3fed594cc83529bf8333eac9af9401
Petr Glinka <glinkap@gmail.com>
2016-08-14 12:17:13 +0300

    no message

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps'); 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+  	.pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./assets/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
